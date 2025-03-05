1ffd90b7c6553fb8b51e20deb071e34a4f8bdfcf
Nona <87380138+nonitut@users.noreply.github.com>
2025-03-05 11:47:56 +0300

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     function handleMouseMove(event){
-        let eyes = document.querySelectorAll(".eye");  // Получаем все элементы с классом .eye
-        eyes.forEach(function(eye){                    //   получаем координаты центра глаза
+        let eyes = document.querySelectorAll(".eye");
+        // получаем все эленты с классом .eye
+        eyes.forEach(function(eye){
             let rect = eye.getBoundingClientRect();
-            // Метод elem.getBoundingClientRect() возвращает координаты 
-            // в контексте окна для минимального по размеру прямоугольника, который заключает в себе элемент elem, 
-            // в виде объекта встроенного класса DOMRect
+            // .getBoundingClientRect() - метод который 
+            // возвращает координаты в контексте окна 
             let eyeX = rect.left + eye.clientWidth / 2;
             let eyeY = rect.top + eye.clientHeight / 2;
-            // вычисляем угол между курсором и центром глаза;
-            let radian = Math.atan2(event.clientX - eyeX, event.clientY - eyeY );
-            let rotation = (radian * (180 / Math.PI) * -1) + 180;
-            // поворачиваем глаз в направлении курсора;
+            // далее вычисляем угол между курсором и центром глаза
+            let radian = Math.atan2(event.clientX - eyeX, event.clientY - eyeY);
+            let rotation = (radian * (180 / Math.PI ) * -1) + 360;
+            // поворачиваем глаза в направлении курсора
             eye.style.transform = `rotate(${rotation}deg)`;
         });
     };
 
-    let moveArea = document.querySelector(".move-area");
-    moveArea.addEventListener("mousemove", handleMouseMove);
+    let moveA = document.querySelector(".move-area");
+    moveA.addEventListener("mousemove", handleMouseMove );
 
-    function cursorColor(event) {
+
+    function sursorX(event){
         let cursor = document.querySelector(".cursor");
-        cursor.style.left = `${event.pageX - 2}px`;
-        cursor.style.top = `${event.pageY - 2}px`;
-    }
-    document.addEventListener("mousemove", cursorColor);
-
+        cursor.style.left = `${event.pageX - 20}px`;
+        cursor.style.top = `${event.pageY - 30}px`;
+    };
+    document.addEventListener("mousemove", sursorX );
 
 
 });
